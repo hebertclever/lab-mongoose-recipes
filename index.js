ef1ff3d87cb6a7875ac143a3e02b7d0b625ef5ef
hebertclever <64936740+hebertclever@users.noreply.github.com>
2020-07-03 02:53:22 -0300

    Completed lab

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
+// const NewRecipe = require('./models/Model.create');
+
 // Import of the data from './data.json'
 const data = require('./data');
 
@@ -21,7 +23,61 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    const result = Recipe.create({
+      title: 'Bife à Parmegiana',
+      level: 'Amateur Chef',
+      ingredients: ['cheese', 'farinha', 'alcatra', 'molho de tomate'],
+      cuisine: 'Italian',
+      dishType: 'main_course',
+      image: 'https://abrilmdemulher.files.wordpress.com/2016/10/receita-bife-a-parmegiana.jpg?quality=90&strip=info',
+      duration: 20,
+      creator: 'Hebert Oliveira',
+      created: '06/04/2019',
+    
+    })
+    .then((data)=> console.log(data))
+    .catch(error => console.log(error))
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  // InsertMany
+  const Title = mongoose.model('Title', { 
+    title: { type: String }, 
+    duration: { type: Number } 
+}); 
+  
+  Title.insertMany([ 
+    { title: 'Almondegas', duration: 20}, 
+    { title: 'Pizza', duration: 20}, 
+    { title: 'Salada', duration: 20} 
+]).then(function(){ 
+    console.log("Data inserted")  
+}).catch(function(error){ 
+    console.log(error)      
+}); 
+
+
+// findOneAndUpdate
+const Creator = mongoose.model('Creator',{ 
+  creator: { type: String }, 
+  }); 
+
+Creator.findOneAndUpdate({duration: {$gte:30} },  
+  {title:"Pizza"}, null, function (err, docs) { 
+  if (err){ 
+      console.log(err) 
+  } 
+  else{ 
+      console.log("Original Doc : ",docs); 
+  } 
+}); 
+
+
+// deleteOne
+Creator.deleteOne({ duration: { $gte: 30 } }).then(function(){ 
+  console.log("Almondegas"); // Success 
+}).catch(function(error){ 
+  console.log(error); // Failure 
+}); 
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,9 +1,57 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const recipeSchema = new Schema({
-  // TODO: write the schema
-});
+  const recipeSchema = new Schema({
+    title: {
+      type: String,
+      required: true,
+    },
+    level: {
+      type: String,
+      enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'],
+      minLength: 10,
+      maxlength: 13,
+    },
+    
+    ingredients: [{ type: String, minLength: 2 }],
+    country: {
+      type: String,
+      match: /^[A-Z][A-Z]$/,
+      
+    cuisine: {
+      type: String,
+      required: true,
+      },
+
+      dishType: {
+      type: String,
+      enum: ['breakfast', 'main_course', 'UltraPro Chef', 'soup',
+       'snack', 'drink', 'dessert'],
+      },
+          
+    },
+    image: {
+      type: String,
+      match: /^https?:\/\//,
+      default:
+        "https://images.media-allrecipes.com/images/75131.jpg",
+
+    duration: {
+      type: Number,
+      min: 0,
+      max: 30,
+      },
+      
+    creator: {
+      type: String,
+    },
+
+    created: { 
+      type: Date,
+      default: Date.now },
+    },
+  });
+
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
